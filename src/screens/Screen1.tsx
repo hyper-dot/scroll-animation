b353c600fcaf89612349122c711011be3fb6538f
Roshan Paudel <rozanpoudel@gmail.com>
2024-04-03 08:57:56 +0545

    text position

--- src/screens/Screen1.tsx
+++ src/screens/Screen1.tsx
@@ -1,5 +1,12 @@
 const Screen1 = () => {
-  return <div className="min-h-screen bg-sky-600"></div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-green-400 text-8xl font-semibold">
+        Hello,
+        <br /> How are you
+      </h1>
+    </div>
+  );
 };
 
 export default Screen1;
--- src/screens/Screen2.tsx
+++ src/screens/Screen2.tsx
@@ -4,19 +4,42 @@ import { useScroll, motion, useTransform } from "framer-motion";
 const Screen2 = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0.4, 0.7], [1, 0.5]);
+
+  /* Right Array side of useTransform is the scroll progress.
+   * When scroll value is reaches 0.4 it starts transforming and when it reaches 0.7 it stops
+   * Left side array is the value of scale factor. It is from 100% when scroll is 0.4 to 50% when scroll is 0.7
+   * */
+  const scale = useTransform(scrollYProgress, [0.4, 0.6], [1, 0.5]);
+  const opacity = useTransform(scrollYProgress, [0.5, 0.58], [0, 1]);
 
   return (
     <div
       ref={containerRef}
-      className="flex items-center h-screen md:h-[300vh] relative w-full"
+      className="h-screen bg-black md:h-[300vh] relative w-full"
     >
       <motion.img
         style={{ scale, transformOrigin: "0 0" }}
         src="https://dynamicmedia.accenture.com/is/image/accenture/A1-A.com-Careers-Module-Image?qlt=85&ts=1711641426964&$1024-PNG$&dpr=off"
-        className="w-full sticky top-10 object-cover h-auto"
+        className="w-full sticky top-32 object-cover h-auto z-10"
       />
-      <div className="h-full bg-red-500"></div>
+      <div className="grid grid-cols-2 relative top-20">
+        <div></div>
+        <motion.div
+          style={{ opacity }}
+          className="md:h-[420px] flex flex-col justify-end"
+        >
+          <div className="max-w-1/2 mx-auto space-y-4 text-white">
+            <h1 className="font-bold">CAREERS</h1>
+            <h2 className="text-4xl font-medium text-green-400">
+              Grow your career at <br /> the heart of change
+            </h2>
+            <p className="text-xl">
+              It's your time to shine. <br /> Bring your ingenuity, curiosity
+              and big ideas.
+            </p>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
